a1a4c5d87ab85e7e61de393eacbc3d0ffd1c92d3
Jake Saferstein <JakeSaf@Jakes-MacBook-Pro-3.local>
2015-04-02 20:12:25 -0700

    Initial Commit

---
+++ PictureThis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PictureThis
+//
+//  Created by Jake Saferstein on 4/2/15.
+//  Copyright (c) 2015 Jake Saferstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PictureThis/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  PictureThis
+//
+//  Created by Jake Saferstein on 4/2/15.
+//  Copyright (c) 2015 Jake Saferstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
